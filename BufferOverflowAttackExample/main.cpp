755667b48c7389b73d35a469025b122a322443b2
Darren Reid <gibgezr@gmail.com>
2017-10-31 15:03:34 -0300

    CHanged passcheck to a bool

--- BufferOverflowAttackExample/main.cpp
+++ BufferOverflowAttackExample/main.cpp
@@ -6,17 +6,17 @@ Based on a standard C-style stack buffer overflow.
 
 void main(void)
 {
-	char passcheck[1] = { 0 };
 	char pwbuffer[9];	
-
+	bool passcheck = false;
+	
 	std::cout << " Address of pwbuffer:\t " << (void *)pwbuffer << "\n";
 	std::cout << " Address of password:\t " << (void *)passcheck << "\n";
-	std::cout << " &passcheck - &pwbuffer = " << (int)passcheck - (int)pwbuffer << "\n";
+	std::cout << " &passcheck - &pwbuffer = " << (int)&passcheck - (int)pwbuffer << "\n";
 	std::cout << "\n-------------------------------------------------------------------\n";
 
 	std::cout << "Please enter your password: ";
 
-	//try entering 'bufferoverflowattackforyou' (any 24 characters will work)
+	//try entering a password 1 character longer than the number printed out for &passcheck - &pwbuffer
 	std::cin >> pwbuffer;
 	
 	std::cout << "\n";
@@ -28,10 +28,10 @@ void main(void)
 	else
 	{
 		std::cout << "Correct password entered.\n";
-		passcheck[0] = 1;
+		passcheck = true;
 	}
 
-	if (passcheck[0])
+	if (passcheck)
 	{
 		//Now our program would allow us to do stuff...
 		std::cout <<  "_-=|Welcome to the inner sanctum, superuser!|=-_ \n";
